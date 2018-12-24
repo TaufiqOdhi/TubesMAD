0d529c875f55bcb44e21fffc1cc16136df3497db
TaufiqOdhi <taufiq_0dhi_dwi_putra@student.uns.ac.id>
2018-12-24 14:07:59 +0700

    Use Geeture Layout

--- app/src/main/java/com/prakmad/tubesmad/MainActivity.java
+++ app/src/main/java/com/prakmad/tubesmad/MainActivity.java
@@ -1,42 +1,44 @@
 package com.prakmad.tubesmad;
 
-import android.support.v4.view.GestureDetectorCompat;
+import android.gesture.Gesture;
+import android.gesture.GestureLibraries;
+import android.gesture.GestureLibrary;
+import android.gesture.GestureOverlayView;
+import android.gesture.Prediction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.GestureDetector;
-import android.view.MotionEvent;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
-    private GestureDetectorCompat mGestureDetector;
+import java.util.ArrayList;
 
-    private class GestureListener extends GestureDetector.SimpleOnGestureListener{
-        @Override
-        public boolean onSingleTapConfirmed(MotionEvent e){
-            Toast.makeText(MainActivity.this, "onSingleTapConfirmed",
-                    Toast.LENGTH_SHORT).show();
-            return super.onSingleTapConfirmed(e);
-        }
-
-        @Override
-        public boolean onDoubleTap(MotionEvent e){
-            Toast.makeText(MainActivity.this,"onDoubleTap",
-                    Toast.LENGTH_SHORT).show();
-            return super.onDoubleTap(e);
-        }
-
-    }
-
-    @Override
-    public boolean onTouchEvent(MotionEvent event){
-        mGestureDetector.onTouchEvent(event);
-        return super.onTouchEvent(event);
-    }
+public class MainActivity extends AppCompatActivity implements GestureOverlayView.OnGesturePerformedListener {
+    GestureLibrary mLibrary;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        mGestureDetector = new GestureDetectorCompat(this, new GestureListener());
+
+        mLibrary = GestureLibraries.fromRawResource(this, R.raw.gestures);
+        if (!mLibrary.load()) {
+            finish();
+        }
+
+        GestureOverlayView gestures = findViewById(R.id.gestures);
+        gestures.addOnGesturePerformedListener(this);
+    }
+
+    public void onGesturePerformed(GestureOverlayView overlay, Gesture gesture) {
+        ArrayList<Prediction> predictions = mLibrary.recognize(gesture);
+
+        if (predictions.size() > 0 && predictions.get(0).score > 1.0) {
+            String result = predictions.get(0).name;
+
+            if ("open".equalsIgnoreCase(result)) {
+                Toast.makeText(this, "Opening the document", Toast.LENGTH_LONG).show();
+            } else if ("save".equalsIgnoreCase(result)) {
+                Toast.makeText(this, "Saving the document", Toast.LENGTH_LONG).show();
+            }
+        }
     }
 }
